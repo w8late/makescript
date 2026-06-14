1905135b2f96c4b7cdf9c5062838be308c4443c7
Denzel Owusu-Sekyere <dudedenzy@gmail.com>
2026-06-14 21:23:32 +1000

    printing sources

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 //import fs from "node:fs/promises";
 
+// show how-to-use screen if given no arguments
+if (process.argv.length < 3) {
+    console.log(
+        "HOW TO USE:\n",
+        "   makescript <FILENAME>.js"
+    );
+    process.exit(0);
+}
 
-console.log("hello!");
+
+const filename = process.argv[2];
+import(`../${filename}`).then((mod) => {
+    console.log(mod.sources);
+});
+
+
+
--- test/test-makescript.js
+++
@@ -1,3 +0,0 @@
-export const sources = [
-    "src/main.c"
-];
